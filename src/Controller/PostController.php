6da5a97e07189fa612f04c300741ae23a8e03f37
greciaD03 <grecia.deyanira.mtz2003@gmail.com>
2023-10-27 11:04:09 -0600

    BASE.v1

---
+++ src/Controller/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostController extends AbstractController
+{
+    #[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods:
+    ['GET', 'HEAD'])]
+    public function index($name): Response
+    {
+        return $this->json([
+            'post' => 'Bienvenidos!',
+            'custom_post' => 'src/Controller/PostController.php'
+        ]);
+    }
+}
