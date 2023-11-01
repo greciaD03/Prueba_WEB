6f2861dbdf839998fe1ec47f5a498ea7c194a301
greciaD03 <grecia.deyanira.mtz2003@gmail.com>
2023-11-01 13:07:19 -0600

    Agrupacion GLOBAL

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -2,26 +2,15 @@
 
 namespace App\Controller;
 
-use App\Enum\OrderStatusEnum;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
-use Symfony\Component\Intl\Languages;
-use Symfony\Component\Intl\Scripts;
-use Symfony\Component\Intl\Countries;
-use Symfony\Component\Intl\Locales;
-use Symfony\Component\Intl\Currencies;
+use App\Enum\OrderStatusEnum;
 
 #[Route('/blog', requirements: ['_locale' => 'en|es|fr'], name: 'blog_')]
 class PostController extends AbstractController
 {
-    /*#[Route('/blog/{page?}', name: 'blog_index', defaults: ['page' => 1, 'title' => 'Hello world!'])]
-    public function index(?int $page, string $title): Response
-    {
-        return new Response ('Sub-Texto: ' .$title);
-    }*/
-
     #[Route('/{_locale}', name: 'index')]
     public function index(): Response
     {
@@ -40,47 +29,53 @@ class PostController extends AbstractController
         return new Response ('Parangaricutirimícuaro');
     }
 
+    #[Route('/{_locale}/doc/{page?}', name: 'blog', defaults: ['page' => 1, 'title' => 'Hello world!'])]
+    public function blog(?int $page, string $title): Response
+    {
+        return new Response ('Sub-Texto: ' .$title);
+    }
+
     /*#[Route('/orders/list/{status}', name: 'list_orders_by_status')]
     public function list(OrderStatusEnum $status = OrderStatusEnum::Paid): Response
     {
         return new Response ('PRUE-ba: ' .$status);
     }*/
     
-    /*#[Route('/blog/{page?}', name: 'blog_list', requirements: ['page' => '2'], priority: 3)]
-    public function list(?int $page=2): Response
+    #[Route('/{_locale}/lista/{page?}', name: 'lista', requirements: ['page' => '2'], priority: 3)]
+    public function lista(?int $page=2): Response
     {
         return new Response ('Pagina: ' .$page); 
-    }*/
+    }
 
-    /*#[Route('/blog/{page3}', name: 'blog_list', requirements: ['page3' => '3'], priority: 2)]
+    #[Route('/{_locale}/parrafo/{page3}', name: 'parrafo', requirements: ['page3' => '3'], priority: 2)]
     public function parrafo(int $page3=3): Response
     {
         return new Response ('Pagina con contador: ' .$page3); 
-    }*/
+    }
 
-    /*#[Route('/blog/{page4}', name: 'blog_list', requirements: ['page4' => '4'])]
+    #[Route('/{_locale}/conjunto/{page4}', name: 'conjunto', requirements: ['page4' => '4'])]
     public function conjunto(int $page4=4): Response
     {
         return new Response ('TEXTO TEXTO TEXO TEXTO: ' .$page4); 
-    }*/
+    }
 
     //"/blog/my-first-post"
 
-    /*#[Route('/blog/{slug}', name: 'blog_show')]
-    public function show($slug='mi-pagina-web'): Response
+    #[Route('/{_locale}/sho/{slug}', name: 'sho')]
+    public function sho($slug='mi-pagina-web', $title='A B C D'): Response
     {
-        return new Response ('NOMBRE de la Pagina: ' .$slug);
-    }*/
+        return new Response ('NOMBRE de la Pagina: ' .$slug .$title);
+    }
 
-    /*#[Route('/blog/{page<\d+>?1}', name: 'blog_list')]
-    public function list(?int $page=1): Response
+    #[Route('/{_locale}/bloqlist/{page<\d+>?1}', name: 'bloqlist')]
+    public function bloqlist(?int $page=1): Response
     {
         return new Response ('T E X T O: ' .$page); 
-    }/*
+    }
 
-   /*#[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods:
+   #[Route('/{_locale}/movies/{name}', name: 'movies', defaults: ['name' => null], methods:
     ['GET', 'HEAD'])]
-    public function index($name): Response
+    public function movies($name): Response
     {
         return $this->json([
             'post' => $name,
@@ -88,9 +83,9 @@ class PostController extends AbstractController
         ]);
     }
 
-    #[Route('/hobbys/{id}', name: 'hobbys', defaults: ['id' => null], methods:
+    #[Route('/{_locale}/hobbys/{id}', name: 'hobbys', defaults: ['id' => null], methods:
     ['GET', 'HEAD'])]
-    public function list($id): Response
+    public function hobbys($id): Response
     {
         return $this->json([
             'post' => $id,
@@ -98,15 +93,15 @@ class PostController extends AbstractController
         ]);
     }
 
-    #[Route('/deportes/{date}', name: 'deportes', defaults: ['date' => null], methods:
+    #[Route('/{_locale}/deportes/{dat}', name: 'deportes', defaults: ['dat' => null], methods:
     ['GET', 'HEAD'])]
-    public function post($date): Response
+    public function deportes($dat): Response
     {
         return $this->json([
-            'post' => $date,
+            'post' => $dat,
             'custom_post' => 'Ruta Deportes'
         ]);
-    }/*
+    }
 
     /*#[Route('/movies/{name}', name: 'movies', defaults: ['name' => null], methods:
     ['PUT'])]
@@ -118,23 +113,23 @@ class PostController extends AbstractController
         ]);
     }*/
 
-    /*#[Route('/movies', name: 'movies')]
-    public function index(): Response
+    #[Route('/{_locale}/mov', name: 'mov')]
+    public function mov(): Response
     {
         return $this->render('post/index.html.twig', [
             'title' => 'Good morning little star´s, the world says HI!'
         ]);
-    }*/
+    }
 
-    /*#[Route('/brecha/{slug}', name: 'brecha')]
+    #[Route('/{_locale}/brecha/{slug}', name: 'brecha')]
     public function brecha(string $slug): Response
     {
         $title = u(str_replace('-', ' ', $slug))->title(true);
         return new Response('Genre: ' .$title);
-    }*/
+    }
 
-    /*#[Route('/brecha/{slug}', name: 'brecha')]
-    public function brecha(string $slug): Response
+    #[Route('/{_locale}/broch/{slug}', name: 'broch')]
+    public function broch(string $slug): Response
     {
         if ($slug) {
             $title = 'Genre: '.u(str_replace('-', ' ', $slug))->title(true);
@@ -142,7 +137,7 @@ class PostController extends AbstractController
             $title = 'All Genres';
         }
         return new Response($title);
-    }*/
+    }
 
     /*#[Route('/browse/{slug}')]
     public function browse(string $slug = null): Response
