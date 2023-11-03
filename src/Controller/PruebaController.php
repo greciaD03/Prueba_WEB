58b12878c5e613eaf514e53c1bde7c22fda5a339
greciaD03 <grecia.deyanira.mtz2003@gmail.com>
2023-11-03 13:37:55 -0600

    Diseño FORM

--- src/Controller/PruebaController.php
+++ src/Controller/PruebaController.php
@@ -8,13 +8,14 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PruebaController extends AbstractController
 {
-    #[Route('/prueba/{$id}', name: 'app_prueba', defaults: ['id' => null])]
-    public function index($id): Response
+    #[Route('/form', name: 'app_form')]
+    public function form($nombre, $usuario, $telefono): Response
     {
         return $this->render('prueba/index.html.twig', [
-            'controller_name' => 'PruebaController',
-            'post' => $id 
-        ]);
+            $nombre,
+            $usuario, 
+            $telefono
+        ]); 
     }
 }
 //https://youtube.com/playlist?list=PLn5IkU1Zhgia_-EHWX97v0gYatn24o7FW&si=PA9YOgykATjUU98X 
